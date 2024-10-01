17741a8fc081163bfadd113a2232e94794ec8dd9
unknown <vv001226@gmail.com>
2024-10-01 19:28:34 -0300

    configs

--- configs/config.go
+++ configs/config.go
@@ -1,1 +1,65 @@
 package configs
+
+import (
+	"github.com/spf13/viper"
+)
+
+var cfg *config
+
+type config struct {
+	API APIConfig
+	DB  DBConfig
+}
+
+type APIConfig struct {
+	Port string
+}
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Pass     string
+	Database string
+}
+
+func init() {
+	viper.setDefault("api.port", "8080")
+	viper.setDefault("database.host", "localhost")
+	viper.setDefault("database.port", "5432")
+}
+
+func Load() error {
+	viper.setConfigName("config")
+	viper.serConfigType("toml")
+	viper.addConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return err
+		}
+	}
+
+	cfg = new(config)
+
+	cfg.API = APIConfig{
+		Port: viper.GetString("api.port"),
+	}
+
+	cfg.DB = DBConfig{
+		Host:     viper.GetString("database.host"),
+		Port:     viper.GetString("database.port"),
+		User:     viper.GetString("database.user"),
+		Pass:     viper.GetString("database.pass"),
+		Database: viper.GetString("database.database"),
+	}
+	return nil
+}
+
+func GetDB() DBConfig {
+	return cfg.DB
+}
+
+func GetServerPort() string {
+	return cfg.API.Port
+}
